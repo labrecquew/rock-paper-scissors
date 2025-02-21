68c6dec924a3d08f49d81e706905a39ea88ff784
labrecquew <labrecque.weston@gmail.com>
2025-02-21 09:30:07 -0500

    Complete the logic to get the computer choice

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let choice = Math.random();
+    let answer = "rock";
+
+    if (choice <= 0.333)
+        answer = "paper";
+    else if (choice <= 0.666)
+        answer = "scissors";
+
+    return answer;
+}
+
